4c72a4b6fc745e2b220753f2d56aa23f18eb08ee
Manpreet Singh <manpreet@yelp.com>
2018-07-20 17:01:14 -0700

    Revert "Remove unused variable"
    This reverts commit bec21504d8f03c58fbb688e22b80a00cff924771.

--- test/testutil.py
+++ test/testutil.py
@@ -9,6 +9,7 @@ import pytest
 from . import unittest
 
 from kafka import SimpleClient, create_message
+from kafka.client_async import KafkaClient
 from kafka.errors import LeaderNotAvailableError, KafkaTimeoutError, InvalidTopicError
 from kafka.structs import OffsetRequestPayload, ProduceRequestPayload, \
                           NotLeaderForPartitionError, UnknownTopicOrPartitionError, \
@@ -118,6 +119,7 @@ class KafkaIntegrationTestCase(unittest.TestCase):
 
         if self.create_client:
             self.client = SimpleClient('%s:%d' % (self.server.host, self.server.port))
+            self.client_async = KafkaClient(bootstrap_servers='%s:%d' % (self.server.host, self.server.port))
 
         timeout = time.time() + 30
         while time.time() < timeout:
