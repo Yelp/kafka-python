f4ac36f08ee46ef09a34d9b5231ee80fdd1f8e4d
Ben Bariteau <bariteau@yelp.com>
2025-09-26 12:12:45 -0700

    add back python 3.8 build and support

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     description="Pure Python client for Apache Kafka",
     long_description=README,
     keywords="apache kafka",
-    python_requires=">=3.10",
+    python_requires=">=3.8",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
